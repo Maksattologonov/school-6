35efd921102b87c410a521d1b39f2b7f8c4776ec
Maksat Tologonov <maksattologonovn@gmail.com>
2022-03-05 04:58:44 +0600

    deploy v119

--- main/settings.py
+++ main/settings.py
@@ -18,7 +18,7 @@ from decouple import config
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
