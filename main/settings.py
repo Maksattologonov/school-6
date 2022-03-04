aa72c77fd1f5db85a4bb2dac536faf3ea56bdb4f
Maksat Tologonov <maksattologonovn@gmail.com>
2022-03-05 05:00:02 +0600

    deploy v120

--- main/settings.py
+++ main/settings.py
@@ -157,6 +157,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
