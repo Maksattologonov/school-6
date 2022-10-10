c9d05abd5f5e15f113c0189fd30e47a6ee02ec3c
makstt <makstt@makstt.host>
2022-10-10 22:25:29 +0600

    fix wrong merge

--- main/settings.py
+++ main/settings.py
@@ -157,9 +157,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "static/",]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
